ee15f09e62ec2296f5e96862bdbef3e98dfbf067
PraNAtaP <pranatapu08@gmail.com>
2026-09-07 02:14:51 +0700

    increment 2: route dan controller transaksi

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class TransactionController extends Controller
+{
+    public function create()
+    {
+        $waktu = now()->format('H:i:s');
+        return "Halaman kasir dibuka pukul {$waktu}";
+    }
+    public function store()
+    {
+        return 'Transaksi disimpan';
+    }
+    public function index()
+    {
+        return 'Daftar transaksi';
+    }
+    public function show(string $id)
+    {
+        return "Detail transaksi #{$id}";
+    }
+    public function riwayat()
+    {
+        return "Riwayat kasir";
+    }
+}
